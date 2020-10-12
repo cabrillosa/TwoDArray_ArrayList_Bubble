f39ed675369d0e48ffc6c5608ff2fad5baf05b83
Artemio Cabrillos JR <act.acabrillos@gmail.com>
2020-10-12 20:00:19 +0800

    Sample 2D array

---
+++ TwoD_array.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class TwoD_array {
+    public static void main(String[] args) {
+        Random rand = new Random();// initialization and Instantiation
+        int num_of_students = 5;
+        int[][] scores = new int[num_of_students][3];
+
+        for (int i = 0; i < num_of_students; i++) {
+            for (int j = 0; j < 3; j++) {
+                scores[i][j] = rand.nextInt(100);
+            }
+        }
+
+        for (int i = 0; i < num_of_students; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.print(scores[i][j]+" ");
+            }
+            System.out.println();
+        }
+    }
+}
